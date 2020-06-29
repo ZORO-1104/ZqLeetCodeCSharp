b1a1cb55d1bb2a7f5e7892aeeb6624d1da22f9b9
ZORO-1104 <zhangqi_1104@163.com>
2020-06-29 09:06:37 +0800

    完善初始框架

---
+++ ZqLeetCodeCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZqLeetCodeCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
