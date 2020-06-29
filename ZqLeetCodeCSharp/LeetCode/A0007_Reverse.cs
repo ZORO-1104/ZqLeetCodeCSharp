f0020cb81ac9b97f2af7815bdbb674d8ec38495f
ZORO-1104 <zhangqi_1104@163.com>
2020-06-29 09:53:24 +0800

    整数反转

---
+++ ZqLeetCodeCSharp/LeetCode/A0007_Reverse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZqLeetCodeCSharp.LeetCode
+{
+    /*
+     * @lc app=leetcode.cn id=7 lang=csharp
+     *
+     * [7] 整数反转
+     */
+
+    // @lc code=start
+    public class A0007_Solution
+    {
+        public int Reverse(int x)
+        {
+            int ret = 0;
+
+            checked
+            {
+                try
+                {
+                    while (x!=0)
+                    {
+                        ret = ret * 10 + x % 10;
+                        x /= 10;
+                    }
+                }
+                catch (Exception)
+                {
+                    ret = 0;
+                }
+            }
+
+            return ret;
+        }
+    }
+    // @lc code=end
+
+
+}
